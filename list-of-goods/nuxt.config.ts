fd712fafbb22a1499f2a236184032886399417d7
Arsam Bagheramiri <arsam5ba5@gmail.com>
2023-05-21 11:28:10 +0330

    packages installed

---
+++ list-of-goods/nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+   css: [
+      //adding the global scss file to nuxt
+      '@/assets/scss/global.scss',
+   ],
+})
